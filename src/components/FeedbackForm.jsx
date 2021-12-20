946f6abe767c24af9431e0a8c3150e10216644c1
Reagan Reuben <reagan.reuben@decagon.dev>
2021-12-20 17:00:40 +0100

    Rating select component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,16 +1,31 @@
 import { useState } from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 
 const FeedbackForm = () => {
 	const [text, setText] = useState('')
-	const handleTextChange = (e) => {
+	const [rating, setRating] = useState(10)
+	const [btnDisabled, setBtnDisabled] = useState(true)
+	const [message, setMessage] = useState('')
+    const handleTextChange = (e) => {
+        if (text === '') {
+            setMessage(null)
+            setBtnDisabled(true)
+        }else if (text !== '' && text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage('Text must be at least 10 characters')
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
 		setText(e.target.value)
 	}
 	return (
 		<Card>
 			<form>
-				<h2>How would you rate our service</h2>
+                <h2>How would you rate our service</h2>
+                <RatingSelect select={(rating) => setRating(rating)} />
 				<div className='input-group'>
 					<input
 						value={text}
@@ -18,10 +33,11 @@ const FeedbackForm = () => {
 						type='text'
 						placeholder='write a review'
 					/>
-					<Button type='submit'>
+					<Button isDisabled={btnDisabled} type='submit'>
 						Send
 					</Button>
-				</div>
+                </div>
+                {message && <div className='message'>{ message}</div>}
 			</form>
 		</Card>
 	)
