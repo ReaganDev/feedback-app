38eca5859077ae9ba7b64da6c798717bbe8d3ebb
Reagan Reuben <reagan.reuben@decagon.dev>
2021-12-20 15:42:20 +0100

    Custom button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,22 +1,30 @@
-import { useState } from "react"
-import Card from "./shared/Card"
+import { useState } from 'react'
+import Card from './shared/Card'
+import Button from './shared/Button'
 
 const FeedbackForm = () => {
-    const [text, setText] = useState('')
-    const handleTextChange = (e) => {
-        setText(e.target.value);
-    }
-    return (
-        <Card>
-            <form>
-                <h2>How would you rate our service</h2>
-                <div className="input-group">
-                    <input value={text} onChange={handleTextChange} type="text" placeholder="write a review"/>
-                    <button type="submit">Send</button>
-                </div>
-            </form>
-        </Card>
-    )
+	const [text, setText] = useState('')
+	const handleTextChange = (e) => {
+		setText(e.target.value)
+	}
+	return (
+		<Card>
+			<form>
+				<h2>How would you rate our service</h2>
+				<div className='input-group'>
+					<input
+						value={text}
+						onChange={handleTextChange}
+						type='text'
+						placeholder='write a review'
+					/>
+					<Button type='submit'>
+						Send
+					</Button>
+				</div>
+			</form>
+		</Card>
+	)
 }
 
 export default FeedbackForm
