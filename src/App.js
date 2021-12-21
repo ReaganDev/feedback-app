fd2447b27afec548f2c411bc81d1a69aa0169140
Reagan Reuben <reagan.reuben@decagon.dev>
2021-12-21 08:12:37 +0100

    Using react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header'
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import { useState } from 'react'
 import FeedbackData from './data/FeedbackData'
+import AboutPage from './components/pages/AboutPage'
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData)
@@ -16,17 +18,28 @@ function App() {
 
 	const addFeedback = (newFeedback) => {
 		feedback.id = uuidv4()
-		setFeedback([newFeedback,...feedback])
+		setFeedback([newFeedback, ...feedback])
 	}
 	return (
-		<>
+		<Router>
 			<Header />
-      <div className='container'>
-        <FeedbackForm handleAdd={addFeedback}/>
-        <FeedbackStats feedback={feedback} />
-				<FeedbackList feedback={feedback} handleDelete={handleDelete} />
+			<div className='container'>
+				<Routes>
+					<Route
+						exact
+						path='/'
+						element={
+							<>
+								<FeedbackForm handleAdd={addFeedback} />
+								<FeedbackStats feedback={feedback} />
+								<FeedbackList feedback={feedback} handleDelete={handleDelete} />
+							</>
+						}
+					></Route>
+					<Route path='/about' element={<AboutPage />} />
+				</Routes>
 			</div>
-		</>
+		</Router>
 	)
 }
 
