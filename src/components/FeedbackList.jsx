0bf157921262dc19957d26cfa2a5e6e548dba57b
Reagan Reuben <reagan.reuben@decagon.dev>
2021-12-21 07:41:26 +0100

    Add animation with framer motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,27 +1,34 @@
-import FeedbackItem from "./FeedbackItem";
+import FeedbackItem from './FeedbackItem'
+import { motion, AnimatePresence } from 'framer-motion'
 import Proptypes from 'prop-types'
 
 const FeedbackList = ({ feedback, handleDelete }) => {
-    if (!feedback || feedback.length === 0) {
-        return <p>No feedback yet</p>
-    }
-    return (
-        <div className="feedback-list">
-            {feedback.map((feed, index) => (
-                <FeedbackItem key={index} item={feed} handleDelete={handleDelete} />
-            ))}
-        </div>
-    )
+	if (!feedback || feedback.length === 0) {
+		return <p>No feedback yet</p>
+	}
+	return (
+		<div className='feedback-list'>
+			<AnimatePresence>
+				{feedback.map((feed, index) => (
+                    <motion.div key={index} initial={{ opacity: 0 }}
+                        animate={{opacity : 1}} exit={{opacity : 0}}
+                    >
+						<FeedbackItem key={index} item={feed} handleDelete={handleDelete} />
+					</motion.div>
+				))}
+			</AnimatePresence>
+		</div>
+	)
 }
 
 FeedbackList.propTypes = {
-    feedback: Proptypes.arrayOf(
-        Proptypes.shape({
-            id: Proptypes.number.isRequired,
-            rating: Proptypes.number.isRequired,
-            text : Proptypes.string.isRequired
-        })
-    )
+	feedback: Proptypes.arrayOf(
+		Proptypes.shape({
+			id: Proptypes.number.isRequired,
+			rating: Proptypes.number.isRequired,
+			text: Proptypes.string.isRequired,
+		})
+	),
 }
 
 export default FeedbackList
