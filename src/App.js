67cc33c113e38660a3da35a62745ce096c8a9d38
Reagan Reuben <reagan.reuben@decagon.dev>
2021-12-21 16:36:53 +0100

    create context and provider

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FeedbackForm from './components/FeedbackForm'
 import { useState } from 'react'
 import FeedbackData from './data/FeedbackData'
 import AboutPage from './components/pages/AboutPage'
+import { FeedbackProvider } from './components/context/FeedbackProvider'
 import AboutIcon from './components/AboutIcon'
 
 function App() {
@@ -22,26 +23,31 @@ function App() {
 		setFeedback([newFeedback, ...feedback])
 	}
 	return (
-		<Router>
-			<Header />
-			<div className='container'>
-				<Routes>
-					<Route
-						exact
-						path='/'
-						element={
-							<>
-								<FeedbackForm handleAdd={addFeedback} />
-								<FeedbackStats feedback={feedback} />
-								<FeedbackList feedback={feedback} handleDelete={handleDelete} />
-							</>
-						}
-					></Route>
-					<Route path='/about' element={<AboutPage />} />
-				</Routes>
-				<AboutIcon />
-			</div>
-		</Router>
+		<FeedbackProvider>
+			<Router>
+				<Header />
+				<div className='container'>
+					<Routes>
+						<Route
+							exact
+							path='/'
+							element={
+								<>
+									<FeedbackForm handleAdd={addFeedback} />
+									<FeedbackStats feedback={feedback} />
+									<FeedbackList
+										feedback={feedback}
+										handleDelete={handleDelete}
+									/>
+								</>
+							}
+						></Route>
+						<Route path='/about' element={<AboutPage />} />
+					</Routes>
+					<AboutIcon />
+				</div>
+			</Router>
+		</FeedbackProvider>
 	)
 }
 
